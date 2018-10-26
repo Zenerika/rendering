33140558b439e314ddead79b7a076583a9aeb1f1
Aguirre <zenerika@ymail.com>
2018-10-25 21:00:39 -0500

    finished with rectangle and circles

--- renderCircles.js
+++ renderCircles.js
@@ -1,32 +1,49 @@
-
-function renderCircles(circles) {
     // HINT: You probably need to write a for loop!
     //       Or, if you're feeling fancy, use .map() 
-    return `
-        <div class="text-center mt-5">
-            <code>${JSON.stringify(circles)}</code>
-        </div>
-    `
+
+
+
+function makeSingleCircle (circle) {
+    return `<div class="circle" style="width:${circle.radius};
+    height:${circle.radius};
+    background:${circle.color};
+    border-radius:50%">
+</div>`
+}
+
+function renderCircles(circles) {
+    return circles.map(makeSingleCircle).join('')
+
+
+    // var i;
+    // var htmlString = ""
+    // for (i = 0; i < circles.length; i++) { 
+    //   htmlString += `<div class="circle" style="width:${circles[i].radius};
+    //                                             height:${circles[i].radius};
+    //                                             background:${circles[i].color};
+    //                                             border-radius:50%">
+    //                 </div>`
+    // }
+    // return htmlString;
 }
 
 function circles() {
-    var content = document.getElementById('content');
-
+    var content = document.getElementById('content');    
     var circlesAbstraction = [
         {
-            radius: 50,
+            radius: '50px',
             color: "#FF00FF"
         },
         {
-            radius: 30,
+            radius: '30px',
             color: "#FF99AA"
         },
         {
-            radius: 60,
+            radius: '60px',
             color: "#0000FF"
         },
         {
-            radius: 10,
+            radius: '10px',
             color: "#000000"
         },
     ];
